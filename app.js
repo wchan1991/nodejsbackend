69f85d7d55ae0c2e9e2c454073d7283a5b482b9d
Will <willchan33@gmail.com>
2018-07-30 14:41:15 -0400

    new_rep

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+var express = require('express');
+var mysql = require("mysql");
+var app = express();
+
+var connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user     : process.env.DB_USER,
+	password : process.env.DB_PASSWORD,
+	database : process.env.DB_NAME
+});
+
+connection.connect(function(error){
+	if (!!error){
+		console.log('Error');
+	} else{
+		console.log('Connected');
+	}
+})
+
+
+app.get('/', function(req,resp){
+	//about mysql
+	connection.query("SElECT * FROM oms_stats", function(error, rows, field){
+		//callback
+		if(!!error){
+			console.log ('error in the query');
+
+		}else{
+			//parse with your rows/fields
+			console.log('Successful Query');
+			resp.send(JSON.stringify(rows));
+
+		}
+	});
+})
+
+app.listen(4041);
